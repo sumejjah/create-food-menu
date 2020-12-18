88692bcbbd53ea0d0deaaa3e490a5b73eafd4c56
sumejjaha+1-at-587105673553 <sumejjaha@maestralsolutions.com>
2020-12-18 09:58:18 +0100

    remove comments

--- src/components/Shared/Loader.js
+++ src/components/Shared/Loader.js
@@ -22,42 +22,26 @@ const Spinner = styled.div`
 
     border-radius: 100%;
     display: inline-block;
-    -webkit-animation: sk-bouncedelay 1.4s infinite ease-in-out both;
     animation: sk-bouncedelay 1.4s infinite ease-in-out both;
-  }
-
-  @-webkit-keyframes sk-bouncedelay {
-    0%,
-    80%,
-    100% {
-      -webkit-transform: scale(0);
-    }
-    40% {
-      -webkit-transform: scale(1);
-    }
   }
 
   @keyframes sk-bouncedelay {
     0%,
     80%,
     100% {
-      -webkit-transform: scale(0);
       transform: scale(0);
     }
     40% {
-      -webkit-transform: scale(1);
       transform: scale(1);
     }
   }
 `;
 
 const Bounce1 = styled.div`
-  -webkit-animation-delay: -0.32s;
   animation-delay: -0.32s;
 `;
 
 const Bounce2 = styled.div`
-  -webkit-animation-delay: -0.16s;
   animation-delay: -0.16s;
 `;
 
